d2aba20fd4b739bfcdf13e88e81d4ed33c70be63
Anuj <anujdas2742006@gmail.com>
2025-12-11 11:26:13 +0530

    new

---
+++ Dynamic Memory Allocation/malloc.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    int *ptr = (int* )malloc(num*sizeof(num));
+
+    // if(ptr == NULL){
+    //     printf("Invalid Malloc!");
+    //     return 1;
+    // }
+    // for(int i =0;i<num;i++){
+    //     printf("Enter %d number: ", i+1);
+    //     scanf("%d", ptr + i);
+    // }
+    // for(int i = 0;i<num;i++){
+    //     printf("%d\n", *(ptr+i));
+    // }
+    printf("%zu", sizeof(*ptr));
+
+    return 0;
+}
